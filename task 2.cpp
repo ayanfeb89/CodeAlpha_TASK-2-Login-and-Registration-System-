1205056b3aad7d6486b6ec212c6d7dcf91a60803
ayanfeb89 <ayanfeb89@gmail.com>
2025-08-22 19:01:24 +0500

    Add files via upload

---
+++ task 2.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    cout<<"Choose any one"<<endl<<"1. Registration"
+    <<endl<<"2. Login"<<endl<<"3. Quit"<<endl;
+    while(true)
+    {
+        int c;
+        cout<<"Enter the choice: ";
+        cin>>c;
+        if(c==1)
+        {
+            string username,password;
+            cout<<"Enter the Username: ";
+            cin.ignore();
+            getline(cin,username);
+            cout<<"Enter the Password: ";
+            cin>>password;
+
+            ifstream inputfile("identity.txt");
+                string u,p;
+                bool found=false;
+                while(inputfile>>u>>p)
+                {
+                    if(u==username)
+                    {
+                        found=true;
+                        break;
+                    }
+                }
+                inputfile.close();
+                if(found)
+                {
+                    cout<<"Error, Username or Password already exist"<<endl;
+                }
+                else{
+                    ofstream outputfile("identity.txt",ios::app);
+                    outputfile<<username<<" "<<password<<endl;
+                    outputfile.close();
+                    cout<<"Registration Complete."<<endl;
+                }
+        }
+        if(c==2)
+        {
+            string username,password;
+            cout<<"Enter the Username: ";
+            cin.ignore();
+            getline(cin,username);
+            cout<<"Enter the password: ";
+            cin>>password;
+
+            ifstream inputfile("identity.txt");
+            string u,p;
+            bool found=false;
+            while(inputfile>>u>>p)
+            {
+                if(u==username && p==password)
+                {
+                    found=true;
+                    break;
+                }
+            }
+            inputfile.close();
+            if(found)
+            {
+                cout<<"Login Successful"<<endl;
+            }
+            else{
+                cout<<"Error, Username or Password not found"<<endl;
+            }
+        }
+        if(c==3)
+        {
+            cout<<"Thank you for visiting this project"<<endl;
+            break;
+        }
+    }
+    return 0;
+}
